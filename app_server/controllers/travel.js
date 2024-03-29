e0fe164f319b1d99fde88b351004dd573c8752f1
= <paul.pathana@snhu.edu>
2024-03-28 20:00:06 -0400

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    console.log('Inside app_server, controllers,travel.js travel function.')
-    res.render('travel', { title: "Travlr Getaways"});
+  console.log('Inside app_server, travel.js travel function.');
+  res.render('travel', { title: 'Travelr Getaways', trips });
 };
 
 module.exports = {
-    travel
+  travel
 };
