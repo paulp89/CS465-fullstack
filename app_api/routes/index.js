fc1992676e34436fbfdd7003788c5cf1603765e7
= <paul.pathana@snhu.edu>
2024-04-11 22:09:53 -0400

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,11 +7,13 @@ const tripsController = require('../controllers/trips');
 // define route for our trips endpoint
 router
   .route('/trips')
-  .get(tripsController.tripsList); // GET Method routes tripsList
+  .get(tripsController.tripsList) // GET Method routes tripsList
+  .post(tripsController.tripsAddTrip); //POST Method Adds a Trip
 
-  //GET Method routes tripsFindByCode - requires parameter
-  router
-      .route('/trips/:tripCode')
-      .get(tripsController.tripsFindByCode);
+//GET Method routes tripsFindByCode - requires parameter
+router
+  .route('/trips/:tripCode')
+  .get(tripsController.tripsFindByCode)
+  .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
