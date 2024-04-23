372fc101fa80933571c6099f2a6991bae521317a
= <paul.pathana@snhu.edu>
2024-04-23 19:38:45 -0400

    Module 7 completed baseline

--- app_admin/src/app/app.config.ts
+++ app_admin/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { authInterceptProvider } from './utils/jwt.interceptors';
+import { authInterceptProvider } from './utils/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- app_admin/src/app/utils/jwt.interceptor.ts
+++ app_admin/src/app/utils/jwt.interceptor.ts
@@ -5,36 +5,38 @@ import { HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 
+
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+constructor(
+private authenticationService: AuthenticationService
+) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let isAuthAPI: boolean;
-
-        // Determine if the request is to an authentication API endpoint
-        if (request.url.startsWith('login') || request.url.startsWith('register')) {
-            isAuthAPI = true;
-        } else {
-            isAuthAPI = false;
-        }
-
-        // Attach the JWT token to the header if the user is logged in and not requesting an auth endpoint
-        if (this.authenticationService.isLoggedIn() && !isAuthAPI) {
-            let token = this.authenticationService.getToken();
-            const authReq = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-
-            return next.handle(authReq);
-        }
-
-        // Forward the request without modification
-        return next.handle(request);
-    }
+intercept(request: HttpRequest<any>, next: HttpHandler):
+Observable<HttpEvent<any>> {
+var isAuthAPI: boolean;
+// console.log('Interceptor::URL' + request.url);
+if(request.url.startsWith('login') ||
+request.url.startsWith('register')) {
+isAuthAPI = true;
 }
+else {
+isAuthAPI = false;
+}
+if(this.authenticationService.isLoggedIn() && !isAuthAPI) {
+let token = this.authenticationService.getToken();
+// console.log(token);
+const authReq = request.clone({
+setHeaders: {
+Authorization: `Bearer ${token}`
+}
+});
+return next.handle(authReq);
+}
+return next.handle(request);
+}
+}
+
 
 
 export const authInterceptProvider: Provider =
